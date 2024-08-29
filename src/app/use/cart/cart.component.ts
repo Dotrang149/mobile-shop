367f4343fec327e50bd34238bafe487d4b1eba80
Oopss <109163684+sang0612@users.noreply.github.com>
2024-08-29 20:16:47 +0700

    fix hôm báo cáo

--- src/app/use/cart/cart.component.ts
+++ src/app/use/cart/cart.component.ts
@@ -16,17 +16,17 @@ export class CartComponent {
 
     {
       product: {
-        name: "Iphone 15",
-        image: "https://sieuthismartphone.com/data/product/small/small_amt1608534646.jpg",
-        price: 100000000
+        name: "Iphone 14",
+        image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQjFqzcDmUbt_IdnFGBbCKIFFaOv0a6Cy4Uj1OZPtYgwAyKS79oZ8yOChDkFRKHBWp_4iSb1-ABkY9GW42j9l_jMfQ2d7NPx_ax5DGRTXTefAbugPdDCyfHpoG7XcrbfUEM3RIC1hzoeg&usqp=CAc",
+        price: 1899.99
       },
       quantity: 2
     },
     {
       product: {
-        name: "Iphone 15 promax",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrUgZqOnXwCjnsKe-BS59j2ib2ApYjs9-SMQ&s",
-        price: 200000000
+        name: "iPhone 12 Pro Max",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ-Mc7JQ-0wEepO8-HanouNTmppOD-qLEV2nOcBenkEoqwLFYSaCUDGPElqQn42Nrp3a9mRTIO5sLGddzIv_2Ffxz7KfdUkEDiNbSnNELzL02cFEyHfb37GUWwHhXXYeX_hM9-4HZMeXOs&usqp=CAc",
+        price: 1099.99
       },
       quantity: 1
     }
